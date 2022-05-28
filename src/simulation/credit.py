d5a7878f9fdb256972725b21925405c889933ee3
NicholasBehr <nicholas.behr.me@gmail.com>
2022-05-28 13:32:13 +0200

    balance due

--- src/simulation/credit.py
+++ src/simulation/credit.py
@@ -6,8 +6,11 @@ class Credit:
 
         Args:
             credit_amount (float): initial amout of money loaned
-            effective_interest (float): yearly effective interest in PERCENT!
-            monthly_rate (float): amount of money returned to creditor every month
+            effective_interest (float): yearly EFFECTIVE INTEREST in PERCENT! 2% -> 2\n
+            The effective interest rate is higher than the nominal interest rate because it takes into account that your 
+            installments are not annual, but monthly. Interest is thus accrued over the course of the year. 
+            The effective interest rate is the interest that you actually pay.
+            monthly_rate (float): amount of money returned to creditor at the end of every month (in arrears)
         """
         self.credit_amount = credit_amount
         self.q_yearly = 1 + (effective_interest/100)
@@ -15,8 +18,26 @@ class Credit:
         self.monthly_rate = monthly_rate
 
     def get_monthly_interest(self) -> float:
-        return (self.q_monthly-1) * 100
+        return (self.q_monthly - 1) * 100
+
+    def get_balance_due(self, after_n_months: int) -> float:
+        """ remaining balace due, after paying n months in arrears \n
+        balance_due_0 \t= credit_amount \n
+        balance_due_1 \t= credit_amount * q_monthly - monthly_rate \n
+        balance_due_2 \t= (credit_amount * q_monthly - monthly_rate) * q_monthly - monthly_rate \n
+                      \t= credit_amount * q_monthly^2 - monthly_rate * (q_monthly^2 + q_monthly^1 + q_monthly^0) \n
+
+        Args:
+            after_n_months (int): after how many months?
+
+        Returns:
+            float: remaining balance due
+        """
+        interest = self.credit_amount * (self.q_monthly ** after_n_months)
+        paid = self.monthly_rate * (1 - (self.q_monthly ** after_n_months)) / (1 - self.q_monthly)
+        return interest - paid
 
 
 if __name__ == '__main__':
-    credit = Credit(10, 1, 2)
+    credit = Credit(10000, 2, 20)
+    print("remaining", credit.get_balance_due(300))
--- tests/test_credit.py
+++ tests/test_credit.py
@@ -1,7 +1,7 @@
 import pytest
 from simulation.credit import Credit as Credit
 
-@pytest.mark.parametrize("yearly_interest,monthly_interest", [
+@pytest.mark.parametrize("yearly_interest, monthly_interest", [
     (1, 0.08295),
     (2, 0.16516),
     (0, 0),
@@ -10,3 +10,13 @@ from simulation.credit import Credit as Credit
 def test_monthly_interest(yearly_interest: float, monthly_interest: float):
     test_credit = Credit(0, yearly_interest, 0)
     assert test_credit.get_monthly_interest() == pytest.approx(monthly_interest, abs=1e-5)
+
+@pytest.mark.parametrize("monthly_rate, balance_due_afer_25_years", [
+    (42.3, -1.02),
+    (10, 12527.32),
+    (100, -22381.37),
+    (0, 16406.06),
+])
+def test_balance_due(monthly_rate: float, balance_due_afer_25_years: float):
+    test_credit = Credit(10000, 2, monthly_rate)
+    assert test_credit.get_balance_due(25*12) == pytest.approx(balance_due_afer_25_years, rel=1e-2)
